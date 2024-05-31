7a38f7718667abb7fe5b19620846b045d706d88c
Lê Quốc Hải <lequochai26@gmail.com>
2024-05-31 10:58:01 +0700

    Added Strings static class for easier strings management and improve reusability.

---
+++ src/Strings.ts
@@ -0,0 +1,11 @@
+export default class Strings {
+    // Static fields:
+    public static NOT_FOUND_TITLE: string = "Oops! Not found!";
+    public static NOT_FOUND_DESCRIPTION: string = "The page you're trying to access may not exists or not available. Please, try again later!";
+    public static NOT_FOUND_IMG_SRC: string = "/icons/not-found.png";
+
+    // Constructors:
+    private constructor() {
+        
+    }
+}
